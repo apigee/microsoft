632a608a2971ef3aac1df2169bccb1945d20bcfe
Sandeep Morusupalli <m.s.a.sandeep@gmail.com>
2016-02-18 23:45:47 -0800

    Update install-apigee.sh

--- azure-apigee-extension/install-apigee.sh
+++ azure-apigee-extension/install-apigee.sh
@@ -38,17 +38,19 @@ curl -o /tmp/setup-org.sh "${FILE_BASEPATH}/setup-org.sh"
 curl -o /tmp/opdk.conf "${FILE_BASEPATH}/opdk.conf"
 curl -o /tmp/CentOS-Base.repo "${FILE_BASEPATH}/CentOS-Base.repo"
 cp -fr /tmp/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo
-cd /tmp
 
-sed -i s/ADMIN_EMAIL=/ADMIN_EMAIL="${USER_NAME}"/g opdk.conf
-sed -i s/APIGEE_ADMINPW=/APIGEE_ADMINPW="${APW}"/g opdk.conf
-sed -i s/APIGEE_LDAPPW=/APIGEE_LDAPPW="${APW}"/g opdk.conf
 
-unzip apigee-edge-${EDGE_VERSION}.zip
-cd apigee-edge-${EDGE_VERSION}
-./apigee-install.sh -j /usr/java/default -r /opt -d /opt
-/opt/apigee4/share/installer/apigee-setup.sh -p aio -f /tmp/opdk.conf
+#cd /tmp
+
+#sed -i s/ADMIN_EMAIL=/ADMIN_EMAIL="${USER_NAME}"/g opdk.conf
+#sed -i s/APIGEE_ADMINPW=/APIGEE_ADMINPW="${APW}"/g opdk.conf
+#sed -i s/APIGEE_LDAPPW=/APIGEE_LDAPPW="${APW}"/g opdk.conf
+
+#unzip apigee-edge-${EDGE_VERSION}.zip
+#cd apigee-edge-${EDGE_VERSION}
+#./apigee-install.sh -j /usr/java/default -r /opt -d /opt
+#/opt/apigee4/share/installer/apigee-setup.sh -p aio -f /tmp/opdk.conf
 #update the setup-org
-cp -fr /tmp/setup-org.sh /opt/apigee4/bin/setup-org.sh
-/opt/apigee4/bin/setup-org.sh ${USER_NAME} ${APW} ${ORG_NAME} ${ENV_NAME} ${VHOST_NAME} ${VHOST_PORT} ${VHOST_ALIAS}
+#cp -fr /tmp/setup-org.sh /opt/apigee4/bin/setup-org.sh
+#/opt/apigee4/bin/setup-org.sh ${USER_NAME} ${APW} ${ORG_NAME} ${ENV_NAME} ${VHOST_NAME} ${VHOST_PORT} ${VHOST_ALIAS}
 
