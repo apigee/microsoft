defd97b3b88a991f5bd5f8e596f2d083679cebbb
Rajesh Mishra <rajeshmi@rajeshmi-macbookpro.roam.corp.google.com>
2018-01-10 13:05:48 -0800

    17x

--- apigee-edge-arm-template/create-solutions.sh
+++ apigee-edge-arm-template/create-solutions.sh
@@ -5,12 +5,15 @@ cp -fr ../azuredeploy.parameters.json mainTemplate.parameters.json
 cp -fr ../createUiDefinition.json .
 cp -fr ../analytics-instances.json .
 cp -fr ../empty-resources.json .
-cp -fr ../install-apigee.sh .
 cp -fr ../internal-instances.json .
+cp -fr ../shared-resources.json .
+cp -fr ../install-apigee.sh .
+cp -fr ../install-portal.sh .
 cp -fr ../management-instances-password.json .
 cp -fr ../management-instances-sshPublicKey.json .
+cp -fr ../devportal-instances-password.json .
+cp -fr ../devportal-instances-sshPublicKey.json .
 cp -fr ../rmp-instances.json .
-cp -fr ../shared-resources.json .
 cp -fr ../metadata.json .
 cp -fr ../README.md .
 cd ..
