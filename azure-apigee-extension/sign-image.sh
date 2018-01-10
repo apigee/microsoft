188c6475efac8d1db4f18ff8b2caed1ad08e1708
Rajesh Mishra <rajeshmi@rajeshmi-macbookpro.roam.corp.google.com>
2018-01-09 20:48:42 -0500

    17x updates

--- azure-apigee-extension/sign-image.sh
+++ azure-apigee-extension/sign-image.sh
@@ -1,3 +1,3 @@
-conn="DefaultEndpointsProtocol=https;AccountName=apigeeedgetrail;AccountKey=XR2H0KS/TXL6IVeUrmD4jcD0YXpXd2OfskrHq1IeRMFbWgwLpUEGqnmkqVsbH3FcPhR1Sqwm8mI5vEibTG4hIw==;EndpointSuffix=core.windows.net"
+conn="DefaultEndpointsProtocol=https;AccountName=apigeeedgetrail;AccountKey=;EndpointSuffix=core.windows.net"
 azure storage container list vhds -c $conn
 azure storage container sas create vhds rl 01/01/2019 -c $conn --start 01/01/2018
