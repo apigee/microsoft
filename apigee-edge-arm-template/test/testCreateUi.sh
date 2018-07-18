01f555909c7c129182c643b3f8d44546965739df
Rajesh Mishra <rajeshmi@google.com>
2018-07-18 11:01:21 -0700

    test ui

---
+++ apigee-edge-arm-template/test/testCreateUi.sh
@@ -0,0 +1,1 @@
+https://ms.portal.azure.com/?clientOptimizations=false#blade/Microsoft_Azure_Compute/CreateMultiVmWizardBlade/internal_bladeCallId/anything/internal_bladeCallerParams/{"initialData":{},"providerConfig":{"createUiDefinition":"https%3A%2F%2Fraw.githubusercontent.com%2Fapigee%2Fmicrosoft%2F17x%2Fapigee-edge-arm-template%2FcreateUiDefinition.json"}}
